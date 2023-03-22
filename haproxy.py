4d2bb323070ac2b54a1637d1b16f127dfae49d94
Gavin Davies <gavin.davies1@digital.hmrc.gov.uk>
2023-03-22 14:52:23 +0000

    TEL-3650 Updates haproxy.py to work with Ubuntu 2204
    References
    --
    
    1. https://jira.tools.tax.service.gov.uk/browse/TEL-3650
    
    Risks
    --
    
    None known
    
    Next steps
    --
    
    1. Update tel-sensu-proxy to consume this version
    2. Roll out tel-sensu-proxy
    
    Collaborators
    --
    
    Co-authored-by: Lee Myring <29373851+thinkstack@users.noreply.github.com>

--- haproxy.py
+++ haproxy.py
@@ -1,3 +1,4 @@
+#! /usr/bin/python3
 # haproxy-collectd-plugin - haproxy.py
 #
 # Author: Michael Leinartas
@@ -8,44 +9,118 @@
 #
 # Modified by "Warren Turkal" <wt@signalfuse.com>, "Volodymyr Zhabiuk" <vzhabiuk@signalfx.com>
 
-import cStringIO as StringIO
 import socket
 import csv
 import pprint
 
 import collectd
+from io import StringIO
 
-PLUGIN_NAME = 'haproxy'
+PLUGIN_NAME = "haproxy"
 RECV_SIZE = 1024
 
 METRICS_TO_COLLECT = {
-    'ConnRate': 'gauge', 'CumReq': 'derive', 'Idle_pct': 'gauge', 'scur': 'gauge', 'SessRate': 'gauge',
-    'lbtot': 'counter', 'bout': 'derive', 'bin': 'derive', 'ttime': 'gauge', 'req_rate': 'gauge', 'rate': 'gauge',
-    'hrsp_2xx': 'derive', 'hrsp_4xx': 'derive', 'hrsp_5xx': 'derive', 'ereq': 'derive', 'dreq': 'derive',
-    'econ': 'derive', 'dresp': 'derive', 'qcur': 'gauge', 'qtime': 'gauge', 'rtime': 'gauge', 'eresp': 'derive',
-    'wretr': 'derive', 'wredis': 'derive', 'MaxConn': 'gauge', 'CumConns': 'derive', 'MaxConnRate': 'gauge',
-    'MaxSessRate': 'gauge', 'MaxSslConns': 'gauge', 'CumSslConns': 'derive', 'MaxPipes': 'gauge', 'Tasks': 'gauge',
-    'Run_queue': 'gauge', 'PipesUsed': 'gauge', 'PipesFree': 'gauge', 'Uptime_sec': 'derive', 'CurrConns': 'gauge',
-    'CurrSslConns': 'gauge', 'SslRate': 'gauge', 'SslFrontendKeyRate': 'gauge', 'SslBackendKeyRate': 'gauge',
-    'SslCacheLookups': 'derive', 'SslCacheMisses': 'derive', 'CompressBpsIn': 'derive', 'CompressBpsOut': 'derive',
-    'ZlibMemUsage': 'gauge', 'chkfail': 'derive', 'downtime': 'derive', 'hrsp_1xx': 'derive', 'hrsp_3xx': 'derive',
-    'hrsp_other': 'derive', 'qmax': 'gauge', 'qlimit': 'gauge', 'rate_lim': 'gauge', 'rate_max': 'gauge',
-    'req_rate_max': 'gauge', 'stot': 'derive', 'slim': 'gauge', 'smax': 'gauge', 'throttle': 'gauge',
-    'cli_abrt': 'derive', 'srv_abrt': 'derive', 'comp_in': 'derive', 'comp_out': 'derive', 'comp_byp': 'derive',
-    'comp_rsp': 'derive', 'ctime': 'gauge', 'act': 'gauge', 'bck': 'gauge', 'check_duration': 'gauge',
-    'lastsess': 'gauge', 'conn_rate': 'gauge', 'conn_rate_max': 'gauge', 'conn_tot': 'counter', 'intercepted': 'gauge',
-    'dcon': 'gauge', 'dses': 'gauge', 'sent': 'gauge', 'snd_error': 'gauge', 'valid': 'gauge', 'update': 'gauge',
-    'cname': 'gauge', 'cname_error': 'gauge', 'any_err': 'gauge', 'nx': 'gauge', 'timeout': 'gauge', 'refused': 'gauge',
-    'other': 'gauge', 'invalid': 'gauge', 'too_big': 'gauge', 'truncated': 'gauge', 'outdated': 'gauge'
+    "ConnRate": "gauge",
+    "CumReq": "derive",
+    "Idle_pct": "gauge",
+    "scur": "gauge",
+    "SessRate": "gauge",
+    "lbtot": "counter",
+    "bout": "derive",
+    "bin": "derive",
+    "ttime": "gauge",
+    "req_rate": "gauge",
+    "rate": "gauge",
+    "hrsp_2xx": "derive",
+    "hrsp_4xx": "derive",
+    "hrsp_5xx": "derive",
+    "ereq": "derive",
+    "dreq": "derive",
+    "econ": "derive",
+    "dresp": "derive",
+    "qcur": "gauge",
+    "qtime": "gauge",
+    "rtime": "gauge",
+    "eresp": "derive",
+    "wretr": "derive",
+    "wredis": "derive",
+    "MaxConn": "gauge",
+    "CumConns": "derive",
+    "MaxConnRate": "gauge",
+    "MaxSessRate": "gauge",
+    "MaxSslConns": "gauge",
+    "CumSslConns": "derive",
+    "MaxPipes": "gauge",
+    "Tasks": "gauge",
+    "Run_queue": "gauge",
+    "PipesUsed": "gauge",
+    "PipesFree": "gauge",
+    "Uptime_sec": "derive",
+    "CurrConns": "gauge",
+    "CurrSslConns": "gauge",
+    "SslRate": "gauge",
+    "SslFrontendKeyRate": "gauge",
+    "SslBackendKeyRate": "gauge",
+    "SslCacheLookups": "derive",
+    "SslCacheMisses": "derive",
+    "CompressBpsIn": "derive",
+    "CompressBpsOut": "derive",
+    "ZlibMemUsage": "gauge",
+    "chkfail": "derive",
+    "downtime": "derive",
+    "hrsp_1xx": "derive",
+    "hrsp_3xx": "derive",
+    "hrsp_other": "derive",
+    "qmax": "gauge",
+    "qlimit": "gauge",
+    "rate_lim": "gauge",
+    "rate_max": "gauge",
+    "req_rate_max": "gauge",
+    "stot": "derive",
+    "slim": "gauge",
+    "smax": "gauge",
+    "throttle": "gauge",
+    "cli_abrt": "derive",
+    "srv_abrt": "derive",
+    "comp_in": "derive",
+    "comp_out": "derive",
+    "comp_byp": "derive",
+    "comp_rsp": "derive",
+    "ctime": "gauge",
+    "act": "gauge",
+    "bck": "gauge",
+    "check_duration": "gauge",
+    "lastsess": "gauge",
+    "conn_rate": "gauge",
+    "conn_rate_max": "gauge",
+    "conn_tot": "counter",
+    "intercepted": "gauge",
+    "dcon": "gauge",
+    "dses": "gauge",
+    "sent": "gauge",
+    "snd_error": "gauge",
+    "valid": "gauge",
+    "update": "gauge",
+    "cname": "gauge",
+    "cname_error": "gauge",
+    "any_err": "gauge",
+    "nx": "gauge",
+    "timeout": "gauge",
+    "refused": "gauge",
+    "other": "gauge",
+    "invalid": "gauge",
+    "too_big": "gauge",
+    "truncated": "gauge",
+    "outdated": "gauge",
 }
 
-DEFAULT_SOCKET = '/var/run/haproxy.sock'
-DEFAULT_PROXY_MONITORS = ['server', 'frontend', 'backend']
+DEFAULT_SOCKET = "/var/run/haproxy.sock"
+DEFAULT_PROXY_MONITORS = ["server", "frontend", "backend"]
 
 
 class HAProxySocket(object):
     """
-            Encapsulates communication with HAProxy via the socket interface
+    Encapsulates communication with HAProxy via the socket interface
     """
 
     def __init__(self, socket_file=DEFAULT_SOCKET):
@@ -53,35 +128,38 @@ class HAProxySocket(object):
 
     def connect(self):
         # unix sockets all start with '/', use tcp otherwise
-        is_unix = self.socket_file.startswith('/')
+        is_unix = self.socket_file.startswith("/")
         if is_unix:
             stat_sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
             stat_sock.connect(self.socket_file)
             return stat_sock
         else:
-            socket_host, separator, port = self.socket_file.rpartition(':')
-            if socket_host != '' and port != '' and separator == ':':
+            socket_host, separator, port = self.socket_file.rpartition(":")
+            if socket_host != "" and port != "" and separator == ":":
                 stat_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 stat_sock.connect((socket_host, int(port)))
                 return stat_sock
             else:
-                collectd.error('Could not connect to socket with host %s. Check HAProxy config.' % self.socket_file)
+                collectd.error(
+                    "Could not connect to socket with host %s. Check HAProxy config."
+                    % self.socket_file
+                )
                 return
 
     def communicate(self, command):
-        '''Get response from single command.
+        """Get response from single command.
 
         Args:
             command: string command to send to haproxy stat socket
 
         Returns:
             a string of the response data
-        '''
-        if not command.endswith('\n'):
-            command += '\n'
+        """
+        if not command.endswith("\n"):
+            command += "\n"
         stat_sock = self.connect()
         if stat_sock is None:
-            return ''
+            return ""
         stat_sock.sendall(command)
         result_buf = StringIO.StringIO()
         buf = stat_sock.recv(RECV_SIZE)
@@ -94,7 +172,7 @@ class HAProxySocket(object):
 
     # This method isn't nice but there's no other way to parse the output of show resolvers from haproxy
     def get_resolvers(self):
-        ''' Gets the resolver config and returns a map of nameserver -> nameservermetrics
+        """Gets the resolver config and returns a map of nameserver -> nameservermetrics
         The output from the socket looks like
         Resolvers section mydns
          nameserver dns1:
@@ -104,20 +182,20 @@ class HAProxySocket(object):
         :return:
         map of nameserver -> nameservermetrics
         e.g. '{dns1': {'sent': '8', ...}, ...}
-        '''
+        """
         result = {}
-        output = self.communicate('show resolvers')
-        nameserver = ''
+        output = self.communicate("show resolvers")
+        nameserver = ""
         for line in output.splitlines():
             try:
-                if 'Resolvers section' in line or line.strip() == '':
+                if "Resolvers section" in line or line.strip() == "":
                     continue
-                elif 'nameserver' in line:
-                    _, unsanitied_nameserver = line.strip().split(' ', 1)
+                elif "nameserver" in line:
+                    _, unsanitied_nameserver = line.strip().split(" ", 1)
                     nameserver = unsanitied_nameserver[:-1]  # remove trailing ':'
                     result[nameserver] = {}
                 else:
-                    key, val = line.split(':', 1)
+                    key, val = line.split(":", 1)
                     current_nameserver_stats = result[nameserver]
                     current_nameserver_stats[key.strip()] = val.strip()
                     result[nameserver] = current_nameserver_stats
@@ -128,10 +206,10 @@ class HAProxySocket(object):
 
     def get_server_info(self):
         result = {}
-        output = self.communicate('show info')
+        output = self.communicate("show info")
         for line in output.splitlines():
             try:
-                key, val = line.split(':', 1)
+                key, val = line.split(":", 1)
             except ValueError:
                 continue
             result[key.strip()] = val.strip()
@@ -139,10 +217,10 @@ class HAProxySocket(object):
         return result
 
     def get_server_stats(self):
-        output = self.communicate('show stat')
+        output = self.communicate("show stat")
         # sanitize and make a list of lines
-        output = output.lstrip('# ').strip()
-        output = [l.strip(',') for l in output.splitlines()]
+        output = output.lstrip("# ").strip()
+        output = [l.strip(",") for l in output.splitlines()]
         csvreader = csv.DictReader(output)
         result = [d.copy() for d in csvreader]
         return result
@@ -150,21 +228,26 @@ class HAProxySocket(object):
 
 def get_stats(module_config):
     """
-        Makes two calls to haproxy to fetch server info and server stats.
-        Returns the dict containing metric name as the key and a tuple of metric value and the dict of dimensions if any
+    Makes two calls to haproxy to fetch server info and server stats.
+    Returns the dict containing metric name as the key and a tuple of metric value and the dict of dimensions if any
     """
-    if module_config['socket'] is None:
-        collectd.error("Socket configuration parameter is undefined. Couldn't get the stats")
+    if module_config["socket"] is None:
+        collectd.error(
+            "Socket configuration parameter is undefined. Couldn't get the stats"
+        )
         return
     stats = []
-    haproxy = HAProxySocket(module_config['socket'])
+    haproxy = HAProxySocket(module_config["socket"])
 
     try:
         server_info = haproxy.get_server_info()
         server_stats = haproxy.get_server_stats()
         resolver_stats = haproxy.get_resolvers()
     except socket.error:
-        collectd.warning('status err Unable to connect to HAProxy socket at %s' % module_config['socket'])
+        collectd.warning(
+            "status err Unable to connect to HAProxy socket at %s"
+            % module_config["socket"]
+        )
         return stats
 
     # server wide stats
@@ -187,24 +270,39 @@ def get_stats(module_config):
     for resolver, resolver_stats in resolver_stats.iteritems():
         for metricname, val in resolver_stats.items():
             try:
-                stats.append((metricname, int(val), {'is_resolver': True, 'nameserver': resolver}))
+                stats.append(
+                    (
+                        metricname,
+                        int(val),
+                        {"is_resolver": True, "nameserver": resolver},
+                    )
+                )
             except (TypeError, ValueError):
                 pass
     return stats
 
 
 def should_capture_metric(statdict, module_config):
-    return (('svname' in statdict and statdict['svname'].lower() in module_config['proxy_monitors']) or
-            ('pxname' in statdict and statdict['pxname'].lower() in module_config['proxy_monitors']) or
-            is_backend_server_metric(statdict) and 'backend' in module_config['proxy_monitors'])
+    return (
+        (
+            "svname" in statdict
+            and statdict["svname"].lower() in module_config["proxy_monitors"]
+        )
+        or (
+            "pxname" in statdict
+            and statdict["pxname"].lower() in module_config["proxy_monitors"]
+        )
+        or is_backend_server_metric(statdict)
+        and "backend" in module_config["proxy_monitors"]
+    )
 
 
 def is_backend_server_metric(statdict):
-    return 'type' in statdict and _get_proxy_type(statdict['type']) == 'server'
+    return "type" in statdict and _get_proxy_type(statdict["type"]) == "server"
 
 
 def is_resolver_metric(statdict):
-    return 'is_resolver' in statdict and statdict['is_resolver']
+    return "is_resolver" in statdict and statdict["is_resolver"]
 
 
 def config(config_values):
@@ -232,26 +330,29 @@ def config(config_values):
             interval = node.values[0]
         elif node.key == "Testing" and node.values[0]:
             testing = _str_to_bool(node.values[0])
-        elif node.key == 'Dimension':
+        elif node.key == "Dimension":
             if len(node.values) == 2:
                 custom_dimensions.update({node.values[0]: node.values[1]})
             else:
-                collectd.warning("WARNING: Check configuration \
-                                            setting for %s" % node.key)
+                collectd.warning(
+                    "WARNING: Check configuration \
+                                            setting for %s"
+                    % node.key
+                )
         else:
-            collectd.warning('Unknown config key: %s' % node.key)
+            collectd.warning("Unknown config key: %s" % node.key)
 
     if not proxy_monitors:
         proxy_monitors += DEFAULT_PROXY_MONITORS
 
     module_config = {
-        'socket': socket,
-        'proxy_monitors': proxy_monitors,
-        'interval': interval,
-        'enhanced_metrics': enhanced_metrics,
-        'excluded_metrics': excluded_metrics,
-        'custom_dimensions': custom_dimensions,
-        'testing': testing,
+        "socket": socket,
+        "proxy_monitors": proxy_monitors,
+        "interval": interval,
+        "enhanced_metrics": enhanced_metrics,
+        "excluded_metrics": excluded_metrics,
+        "custom_dimensions": custom_dimensions,
+        "testing": testing,
     }
     proxys = "_".join(proxy_monitors)
 
@@ -260,63 +361,71 @@ def config(config_values):
 
     interval_kwarg = {}
     if interval:
-        interval_kwarg['interval'] = interval
-    collectd.register_read(collect_metrics, data=module_config,
-                           name='node_' + module_config['socket'] + '_' + proxys,
-                           **interval_kwarg)
+        interval_kwarg["interval"] = interval
+    collectd.register_read(
+        collect_metrics,
+        data=module_config,
+        name="node_" + module_config["socket"] + "_" + proxys,
+        **interval_kwarg
+    )
 
 
 def _format_plugin_instance(dimensions):
     if is_backend_server_metric(dimensions):
-        return "{0}.{1}.{2}".format("backend", dimensions['pxname'].lower(), dimensions['svname'])
+        return "{0}.{1}.{2}".format(
+            "backend", dimensions["pxname"].lower(), dimensions["svname"]
+        )
     elif is_resolver_metric(dimensions):
-        return "nameserver.{0}".format(dimensions['nameserver'])
+        return "nameserver.{0}".format(dimensions["nameserver"])
     else:
-        return "{0}.{1}".format(dimensions['svname'].lower(), dimensions['pxname'])
+        return "{0}.{1}".format(dimensions["svname"].lower(), dimensions["pxname"])
 
 
 def _get_proxy_type(type_id):
     """
-        Return human readable proxy type
-        Args:
-        type_id: 0=frontend, 1=backend, 2=server, 3=socket/listener
+    Return human readable proxy type
+    Args:
+    type_id: 0=frontend, 1=backend, 2=server, 3=socket/listener
     """
     proxy_types = {
-        0: 'frontend',
-        1: 'backend',
-        2: 'server',
-        3: 'socket/listener',
+        0: "frontend",
+        1: "backend",
+        2: "server",
+        3: "socket/listener",
     }
     return proxy_types.get(int(type_id))
 
 
 def _str_to_bool(val):
-    '''
+    """
     Converts a true/false string to a boolean
-    '''
+    """
     val = str(val).strip().lower()
-    if val == 'true':
+    if val == "true":
         return True
-    elif val != 'false':
-        collectd.warning('Warning: String (%s) could not be converted to a boolean. Returning false.' % val)
+    elif val != "false":
+        collectd.warning(
+            "Warning: String (%s) could not be converted to a boolean. Returning false."
+            % val
+        )
 
     return False
 
 
 def submit_metrics(metric_datapoint):
     datapoint = collectd.Values()
-    datapoint.type = metric_datapoint['type']
-    datapoint.type_instance = metric_datapoint['type_instance']
-    datapoint.plugin = metric_datapoint['plugin']
-    if 'plugin_instance' in metric_datapoint.keys():
-        datapoint.plugin_instance = metric_datapoint['plugin_instance']
-    datapoint.values = metric_datapoint['values']
+    datapoint.type = metric_datapoint["type"]
+    datapoint.type_instance = metric_datapoint["type_instance"]
+    datapoint.plugin = metric_datapoint["plugin"]
+    if "plugin_instance" in metric_datapoint.keys():
+        datapoint.plugin_instance = metric_datapoint["plugin_instance"]
+    datapoint.values = metric_datapoint["values"]
     collectd.debug(pprint.pformat(metric_datapoint))
     datapoint.dispatch()
 
 
 def collect_metrics(module_config):
-    collectd.debug('beginning collect_metrics')
+    collectd.debug("beginning collect_metrics")
     """
         A callback method that gets metrics from HAProxy and records them to collectd.
     """
@@ -324,23 +433,25 @@ def collect_metrics(module_config):
     info = get_stats(module_config)
 
     if not info:
-        collectd.warning('%s: No data received' % PLUGIN_NAME)
+        collectd.warning("%s: No data received" % PLUGIN_NAME)
         return
 
     for metric_name, metric_value, dimensions in info:
         # assert metric is in valid metrics lists
         if metric_name not in METRICS_TO_COLLECT:
-            collectd.debug("metric %s is not in list of metrics to collect" % metric_name.lower())
+            collectd.debug(
+                "metric %s is not in list of metrics to collect" % metric_name.lower()
+            )
             continue
 
         metric_datapoint = {
-                    'plugin': PLUGIN_NAME,
-                    'type': METRICS_TO_COLLECT[metric_name],
-                    'type_instance': metric_name.lower(),
-                    'values': (metric_value,)
-                }
+            "plugin": PLUGIN_NAME,
+            "type": METRICS_TO_COLLECT[metric_name],
+            "type_instance": metric_name.lower(),
+            "values": (metric_value,),
+        }
         if len(dimensions) > 0:
-            metric_datapoint['plugin_instance'] = _format_plugin_instance(dimensions)
+            metric_datapoint["plugin_instance"] = _format_plugin_instance(dimensions)
         collectd.debug(pprint.pformat(metric_datapoint))
         submit_metrics(metric_datapoint)
 
